03457692d2f5527a34ae7cef15531de618de216b
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-23 19:56:39 -0500

    placeholder

--- 2024/guessing_game/src/main.rs
+++ 2024/guessing_game/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing GAME !!!!");
+    println!("Guess the number!");
+    let mut guess = String::new();
+    let name = "lenin"; //inmutable
+    let mut last_name = "acosta"; //mutable
+    io::stdin()
+    .read_line(& mut guess)
+    .expect("failes to read line...");
+    println!("you guessed the number: {guess}");
+    placeholders();
+}
+
+fn placeholders(){
+    let x = 1;
+    let y = 10;
+    println!("x = {x} and x + y = {}",y + x);
 }
