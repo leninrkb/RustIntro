debb3b4b6a0fb7c79a91be0ac39242fb7b01e106
Lenin Acosta <rkb.lenin@gmail.com>
2024-03-23 21:24:46 -0500

    match and parsing

--- 2024/guessing_game/src/main.rs
+++ 2024/guessing_game/src/main.rs
@@ -1,6 +1,16 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    // first_sample();
+    // placeholders();
+    comparing_secret_number();
+}
+
+fn first_sample(){
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("the secret number is {secret_number}");
     println!("Welcome to the guessing GAME !!!!");
     println!("Guess the number!");
     let mut guess = String::new();
@@ -10,11 +20,25 @@ fn main() {
     .read_line(& mut guess)
     .expect("failes to read line...");
     println!("you guessed the number: {guess}");
-    placeholders();
 }
 
 fn placeholders(){
     let x = 1;
     let y = 10;
     println!("x = {x} and x + y = {}",y + x);
+}
+
+fn comparing_secret_number(){
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut user_input = String::new();
+    println!("write a number:");
+    io::stdin()
+    .read_line(&mut user_input)
+    .expect("failed to read line");
+    let user_input: u32 = user_input.trim().parse().expect("please write a NUMBER you stupid piece of shit :) ");
+    match user_input.cmp(&secret_number){
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("you win!"),
+    }
 }
