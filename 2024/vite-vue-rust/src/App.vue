5cec6903bc21853923a0e49ffbdf29f26d075391
Base Oracle DBS. Subtransmision <rkb.lenin@gmail.com>
2024-04-16 17:58:15 -0500

    scrapping app structure

--- 2024/vite-vue-rust/src/App.vue
+++ 2024/vite-vue-rust/src/App.vue
@@ -1,37 +1,7 @@
 <template>
-  <RowCenter>
-    <h1 class="text-2xl text-teal-500">Vite + Vue + Rust</h1>
-  </RowCenter>
-  <RowCenter>
-    <Button class="w-fit" @click="confirmar">
-      {{btn_texto}}
-    </Button>
-  </RowCenter>
-  <RowCenter>
-    <p v-if="toggle" class="text-sm">
-      Confirmen un smite en la noche!
-    </p>
-  </RowCenter>
+    <HomeView></HomeView>
 </template>
 
-
 <script setup>
-import RowCenter from './components/layout/RowCenter.vue';
-import Button from './components/UI/Button.vue';
-import { ref, onMounted } from 'vue';
-
-let btn_texto = ref('');
-let toggle = ref(true);
-
-let confirmar = () => {
-  btn_texto.value = toggle.value ? 'avisar' : 'confirmar';
-  toggle.value = !toggle.value;
-}
-
-onMounted(() => {
-  confirmar();
-});
-
+import HomeView from './views/HomeView.vue';
 </script>
-
-
---
+++ 2024/vite-vue-rust/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+    <Column>
+        <div>
+            <!-- header -->
+        </div>
+        <div>
+            <!-- search -->
+        </div>
+        <div>
+            <!-- options -->
+        </div>
+        <div>
+            <!-- content -->
+            <Column>
+                <div>
+                    <!-- web page view -->
+                </div>
+                <div>
+                    <!-- html view -->
+                </div>
+            </Column>
+        </div>
+    </Column>
+</template>
+
+<script setup>
+import Column from '../components/layout/Column.vue';
+</script>
