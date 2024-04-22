0f71721b7078022e8ce28d5267e7c711deefc72e
Base Oracle DBS. Subtransmision <rkb.lenin@gmail.com>
2024-04-22 15:21:27 -0500

    Update result.rs

--- 2024/error_handling/src/result.rs
+++ 2024/error_handling/src/result.rs
@@ -4,32 +4,37 @@ use std::io::Error;
 
 pub fn body() {
     let file = File::open("../maifn.rs");
-    error_handler(&file);
+    // error_handler(&file);
     error_handler_fully(&file);
 }
 
 fn error_handler(file: &Result<File, Error>) {
     match file {
-        Result::Ok(ok) => println!("tudu ben"),
-        Result::Err(error) => println!("error bro"),
+        Result::Ok(ok) => println!("everything's fine..."),
+        Result::Err(error) => println!("holy crab..."),
     }
 }
 
-fn error_handler_fully(file: &Result<File, Error>) {
-    match file {
-        Result::Ok(_file) => (),
-        Result::Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("main_test.rs") {
-                Result::Ok(f) => { 
-                    println!("tudu bien mao mio");
+fn error_handler_fully(result: &Result<File, Error>) {
+    match result {
+        Result::Ok(file) => {
+            println!("file is ok...");
+        },
+        Result::Err(error) => {
+            match error.kind() {
+                ErrorKind::NotFound => {
+                    let created = File::create("delete_this_file.txt");
+                    match created {
+                        Ok(ok) => {
+                            println!("file created succesfully");
+                        },
+                        Err(err) => {
+                            println!("file cannot be created...");
+                        },
+                    }
                 },
-                Result::Err(error) => {
-                    println!("[ERROR]: no pude crear el archivo bro :( disculpame");
-                },
-            },
-            other_error => {
-                panic!("[ERROR]: juesumadre llamen a dios!")
+                other => println!("there;s an error..."),
             }
         },
-    }
+    } 
 }
